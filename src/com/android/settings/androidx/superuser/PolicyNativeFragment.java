20bcefa40f26eb938545f9e31559044fb88ba10e
Koushik Dutta <koushd@gmail.com>
2013-02-28 14:49:24 -0800

    Switch to new Superuser
    Change-Id: I9d91392ad6fbc9953d19059ab59b91d214c00e0c
    
    Conflicts:
    	Android.mk
    	proguard.flags
    
    Conflicts:
    	proguard.flags
    
    Conflicts:
    	proguard.flags

---
+++ src/com/android/settings/androidx/superuser/PolicyNativeFragment.java
@@ -0,0 +1,4 @@
+package com.android.settings.androidx.superuser;
+
+public class PolicyNativeFragment extends com.koushikdutta.superuser.PolicyNativeFragment {
+}
---
+++ src/com/android/settings/androidx/superuser/RequestActivity.java
@@ -0,0 +1,4 @@
+package com.android.settings.androidx.superuser;
+
+public class RequestActivity extends com.koushikdutta.superuser.RequestActivity {
+}
