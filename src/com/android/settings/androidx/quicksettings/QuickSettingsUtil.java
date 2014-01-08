fcd0841af12319a09c477a4f37fa67adae91ac35
Jubakuba <Jubakuba@gmail.com>
2014-01-08 04:07:56 -0700

    Settings: Favorite Contact Tile (2/2)
    PSX: Create Dependant
    
    Change-Id: I938490f3cfd40d1abff193315f5d885010386107
    
    Conflicts:
    	src/com/android/settings/androidx/quicksettings/QuickSettingsTiles.java

--- src/com/android/settings/androidx/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/androidx/quicksettings/QuickSettingsUtil.java
@@ -26,6 +26,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_BATTERY;
 import static com.android.internal.util.slim.QSConstants.TILE_BLUETOOTH;
 import static com.android.internal.util.slim.QSConstants.TILE_BRIGHTNESS;
 import static com.android.internal.util.slim.QSConstants.TILE_BUGREPORT;
+import static com.android.internal.util.slim.QSConstants.TILE_CONTACT;
 import static com.android.internal.util.slim.QSConstants.TILE_CUSTOM;
 import static com.android.internal.util.slim.QSConstants.TILE_CUSTOM_KEY;
 import static com.android.internal.util.slim.QSConstants.TILE_CUSTOM_DELIMITER;
@@ -150,6 +151,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_CUSTOM, R.string.title_tile_custom,
                 "com.android.systemui:drawable/ic_qs_settings"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_CONTACT, R.string.title_tile_contact,
+                "com.android.systemui:drawable/ic_qs_default_user"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
@@ -285,6 +289,8 @@ Log.e("qsUtil", "finally removed current tile: " + id);
                 Settings.System.CUSTOM_TOGGLE_EXTRAS, null);
         Settings.System.putString(context.getContentResolver(),
                 Settings.System.CUSTOM_TOGGLE_ACTIONS, null);
+        Settings.System.putString(context.getContentResolver(),
+                Settings.System.TILE_CONTACT_ACTIONS, null);
     }
 
     public static void deleteCustomTile(Context context, String tileKey) {
@@ -408,8 +414,7 @@ Log.e("qsUtil", "finally removed current tile: " + id);
     }
 
     public static void saveCustomExtras(
-            Context context, String action, String tilekey) {
-        String setting = Settings.System.CUSTOM_TOGGLE_EXTRAS;
+            Context context, String action, String tilekey, String setting) {
         String oldSetting = Settings.System.getString(
                 context.getContentResolver(),
                 setting);
