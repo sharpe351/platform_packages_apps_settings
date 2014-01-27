SlimRoms/packages_apps_Settings

Settings: Port Lockscreen Targets and Shortcuts to SlimKat
First thanks to PA and CM which gave us partly code and some ideas.
Still some parts of the original implementation are/were buggy as well
we needed to ehance it.

Whatever the new Slim version is now ready and seems to be completly bug
free.

- Configurable lockscreen targets...on tablets 8 on phone either 5 or 8
- configurable lockscreen shortcuts only on phones...up to 5 shortcuts
- Centralazing some methods....using now then rounded corner on custom
  images systemwide...means on all other shorcut features we have
- multiDpi compatible
- new settings logic
- instance handling of all dialogs like we do on all other places
  way nicer this way
- tons of fixes

thanks kroz for redoing all xxhdpi icon in fb

Change-Id: I7749c592f75e51faa5db84d0a0ec314623c9b499

src/com/android/settings/slim/LockscreenInterface.java
@@ -1,0 +1,191 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.slim;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceCategory;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LockscreenInterface extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "LockscreenInterface";
+
+    private static final int DLG_ENABLE_EIGHT_TARGETS = 0;
+
+    private static final String PREF_LOCKSCREEN_EIGHT_TARGETS = "lockscreen_eight_targets";
+    private static final String PREF_LOCKSCREEN_SHORTCUTS = "lockscreen_shortcuts";
+
+    private CheckBoxPreference mLockscreenEightTargets;
+    private Preference mShortcuts;
+
+    private boolean mCheckPreferences;
+
+    public boolean hasButtons() {
+        return !getResources().getBoolean(com.android.internal.R.bool.config_showNavigationBar);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        createCustomLockscreenView();
+    }
+
+    private PreferenceScreen createCustomLockscreenView() {
+        mCheckPreferences = false;
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
+
+        addPreferencesFromResource(R.xml.lockscreen_interface_settings);
+        prefs = getPreferenceScreen();
+
+        mLockscreenEightTargets = (CheckBoxPreference) findPreference(
+                PREF_LOCKSCREEN_EIGHT_TARGETS);
+        mLockscreenEightTargets.setChecked(Settings.System.getInt(
+                getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_EIGHT_TARGETS, 0) == 1);
+        mLockscreenEightTargets.setOnPreferenceChangeListener(this);
+
+        mShortcuts = (Preference) findPreference(PREF_LOCKSCREEN_SHORTCUTS);
+        mShortcuts.setEnabled(!mLockscreenEightTargets.isChecked());
+
+        mCheckPreferences = true;
+        return prefs;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        createCustomLockscreenView();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (!mCheckPreferences) {
+            return false;
+        }
+        if (preference == mLockscreenEightTargets) {
+            showDialogInner(DLG_ENABLE_EIGHT_TARGETS, (Boolean) objValue);
+            return true;
+        }
+        return false;
+    }
+
+
+    private void showDialogInner(int id, boolean state) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id, state);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id, boolean state) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            args.putBoolean("state", state);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        LockscreenInterface getOwner() {
+            return (LockscreenInterface) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            final boolean state = getArguments().getBoolean("state");
+            switch (id) {
+                case DLG_ENABLE_EIGHT_TARGETS:
+                    String message = getOwner().getResources()
+                                .getString(R.string.lockscreen_enable_eight_targets_dialog);
+                    if (state) {
+                        message = message + " " + getOwner().getResources().getString(
+                                R.string.lockscreen_enable_eight_targets_enabled_dialog);
+                    }
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.attention)
+                    .setMessage(message)
+                    .setNegativeButton(R.string.dlg_cancel,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    })
+                    .setPositiveButton(R.string.dlg_ok,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().getContentResolver(),
+                                    Settings.System.LOCKSCREEN_EIGHT_TARGETS, state ? 1 : 0);
+                            getOwner().mShortcuts.setEnabled(!state);
+                            Settings.System.putString(getOwner().getContentResolver(),
+                                    Settings.System.LOCKSCREEN_TARGETS, null);
+                            for (File pic : getOwner().getActivity().getFilesDir().listFiles()) {
+                                if (pic.getName().startsWith("lockscreen_")) {
+                                    pic.delete();
+                                }
+                            }
+                            if (state) {
+                                Toast.makeText(getOwner().getActivity(),
+                                        R.string.lockscreen_target_reset,
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            int id = getArguments().getInt("id");
+            boolean state = getArguments().getBoolean("state");
+            switch (id) {
+                case DLG_ENABLE_EIGHT_TARGETS:
+                    getOwner().mLockscreenEightTargets.setChecked(!state);
+                    break;
+             }
+        }
+    }
+
+}

src/com/android/settings/slim/fragments/LockscreenShortcutFragment.java
@@ -1,0 +1,80 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.slim.fragments;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class LockscreenShortcutFragment extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS =
+            "lockscreen_shortcuts_longpress";
+
+    private CheckBoxPreference mLockscreenShortcutsLongpress;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.lockscreen_shortcut_fragment);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mLockscreenShortcutsLongpress = (CheckBoxPreference) findPreference(
+                PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS);
+        mLockscreenShortcutsLongpress.setChecked(Settings.System.getInt(
+                getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS, 1) == 1);
+        mLockscreenShortcutsLongpress.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        // our container already takes care of the padding
+        if (list != null) {
+            int paddingTop = list.getPaddingTop();
+            int paddingBottom = list.getPaddingBottom();
+            list.setPadding(0, paddingTop, 0, paddingBottom);
+        }
+        return view;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLockscreenShortcutsLongpress) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS,
+                    (Boolean) newValue ? 0 : 1);
+        }
+        return true;
+    }
+}

sharpe351/platform_packages_apps_settings

Conflicts:
	res/values/custom_arrays.xml
	res/xml/security_settings_chooser.xml
	src/com/android/settings/SecuritySettings.java
	src/com/android/settings/androidx/util/ShortcutPickerHelper.java

src/com/android/settings/androidx/LockscreenInterface.java
@@ -1,0 +1,191 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.androidx;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceCategory;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LockscreenInterface extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "LockscreenInterface";
+
+    private static final int DLG_ENABLE_EIGHT_TARGETS = 0;
+
+    private static final String PREF_LOCKSCREEN_EIGHT_TARGETS = "lockscreen_eight_targets";
+    private static final String PREF_LOCKSCREEN_SHORTCUTS = "lockscreen_shortcuts";
+
+    private CheckBoxPreference mLockscreenEightTargets;
+    private Preference mShortcuts;
+
+    private boolean mCheckPreferences;
+
+    public boolean hasButtons() {
+        return !getResources().getBoolean(com.android.internal.R.bool.config_showNavigationBar);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        createCustomLockscreenView();
+    }
+
+    private PreferenceScreen createCustomLockscreenView() {
+        mCheckPreferences = false;
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
+
+        addPreferencesFromResource(R.xml.lockscreen_interface_settings);
+        prefs = getPreferenceScreen();
+
+        mLockscreenEightTargets = (CheckBoxPreference) findPreference(
+                PREF_LOCKSCREEN_EIGHT_TARGETS);
+        mLockscreenEightTargets.setChecked(Settings.System.getInt(
+                getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_EIGHT_TARGETS, 0) == 1);
+        mLockscreenEightTargets.setOnPreferenceChangeListener(this);
+
+        mShortcuts = (Preference) findPreference(PREF_LOCKSCREEN_SHORTCUTS);
+        mShortcuts.setEnabled(!mLockscreenEightTargets.isChecked());
+
+        mCheckPreferences = true;
+        return prefs;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        createCustomLockscreenView();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (!mCheckPreferences) {
+            return false;
+        }
+        if (preference == mLockscreenEightTargets) {
+            showDialogInner(DLG_ENABLE_EIGHT_TARGETS, (Boolean) objValue);
+            return true;
+        }
+        return false;
+    }
+
+
+    private void showDialogInner(int id, boolean state) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id, state);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id, boolean state) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            args.putBoolean("state", state);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        LockscreenInterface getOwner() {
+            return (LockscreenInterface) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            final boolean state = getArguments().getBoolean("state");
+            switch (id) {
+                case DLG_ENABLE_EIGHT_TARGETS:
+                    String message = getOwner().getResources()
+                                .getString(R.string.lockscreen_enable_eight_targets_dialog);
+                    if (state) {
+                        message = message + " " + getOwner().getResources().getString(
+                                R.string.lockscreen_enable_eight_targets_enabled_dialog);
+                    }
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.attention)
+                    .setMessage(message)
+                    .setNegativeButton(R.string.dlg_cancel,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    })
+                    .setPositiveButton(R.string.dlg_ok,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().getContentResolver(),
+                                    Settings.System.LOCKSCREEN_EIGHT_TARGETS, state ? 1 : 0);
+                            getOwner().mShortcuts.setEnabled(!state);
+                            Settings.System.putString(getOwner().getContentResolver(),
+                                    Settings.System.LOCKSCREEN_TARGETS, null);
+                            for (File pic : getOwner().getActivity().getFilesDir().listFiles()) {
+                                if (pic.getName().startsWith("lockscreen_")) {
+                                    pic.delete();
+                                }
+                            }
+                            if (state) {
+                                Toast.makeText(getOwner().getActivity(),
+                                        R.string.lockscreen_target_reset,
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            int id = getArguments().getInt("id");
+            boolean state = getArguments().getBoolean("state");
+            switch (id) {
+                case DLG_ENABLE_EIGHT_TARGETS:
+                    getOwner().mLockscreenEightTargets.setChecked(!state);
+                    break;
+             }
+        }
+    }
+
+}

src/com/android/settings/androidx/fragments/LockscreenShortcutFragment.java
@@ -1,0 +1,80 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.androidx.fragments;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class LockscreenShortcutFragment extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS =
+            "lockscreen_shortcuts_longpress";
+
+    private CheckBoxPreference mLockscreenShortcutsLongpress;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.lockscreen_shortcut_fragment);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mLockscreenShortcutsLongpress = (CheckBoxPreference) findPreference(
+                PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS);
+        mLockscreenShortcutsLongpress.setChecked(Settings.System.getInt(
+                getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS, 1) == 1);
+        mLockscreenShortcutsLongpress.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        // our container already takes care of the padding
+        if (list != null) {
+            int paddingTop = list.getPaddingTop();
+            int paddingBottom = list.getPaddingBottom();
+            list.setPadding(0, paddingTop, 0, paddingBottom);
+        }
+        return view;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLockscreenShortcutsLongpress) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS,
+                    (Boolean) newValue ? 0 : 1);
+        }
+        return true;
+    }
+}