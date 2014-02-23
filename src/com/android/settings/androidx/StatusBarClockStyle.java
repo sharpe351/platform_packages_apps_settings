07bed4d52b1dee7bcfc0eab005e968be1d207def
TheMrcool212 <munirmohamadrani@gmail.com>
2014-02-23 16:59:03 +0530

    [2/2] OmniGears: Clock font style
    Base: https://gerrit.omnirom.org/#/c/5760/
    
    Change-Id: Ie607d313f36f30b66b38d7e605a799baa34a37b0
    
    Conflicts:
    	res/values/custom_arrays.xml
    	res/values/custom_strings.xml
    	src/org/androidx/settings/StatusBarClockStyle.java
    
    Conflicts:
    	res/values/arrays.xml
    	res/values/strings.xml

--- src/com/android/settings/androidx/StatusBarClockStyle.java
+++ src/com/android/settings/androidx/StatusBarClockStyle.java
@@ -52,6 +52,7 @@ public class StatusBarClockStyle extends SettingsPreferenceFragment
     private static final String PREF_ENABLE = "clock_style";
     private static final String PREF_AM_PM_STYLE = "status_bar_am_pm";
     private static final String PREF_COLOR_PICKER = "clock_color";
+    private static final String PREF_FONT_STYLE = "font_style";
     private static final String PREF_CLOCK_DATE_DISPLAY = "clock_date_display";
     private static final String PREF_CLOCK_DATE_STYLE = "clock_date_style";
     private static final String PREF_CLOCK_DATE_FORMAT = "clock_date_format";
@@ -66,6 +67,7 @@ public class StatusBarClockStyle extends SettingsPreferenceFragment
     private ListPreference mClockStyle;
     private ListPreference mClockAmPmStyle;
     private ColorPickerPreference mColorPicker;
+    private ListPreference mClockFontStyle;
     private ListPreference mClockDateDisplay;
     private ListPreference mClockDateStyle;
     private ListPreference mClockDateFormat;
@@ -116,6 +118,13 @@ public class StatusBarClockStyle extends SettingsPreferenceFragment
             mColorPicker.setSummary(hexColor);
         }
         mColorPicker.setNewPreviewColor(intColor);
+
+        mClockFontStyle = (ListPreference) findPreference(PREF_FONT_STYLE);
+        mClockFontStyle.setOnPreferenceChangeListener(this);
+        mClockFontStyle.setValue(Integer.toString(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.STATUSBAR_CLOCK_FONT_STYLE,
+                4)));
+        mClockFontStyle.setSummary(mClockFontStyle.getEntry());
 
         mClockDateDisplay = (ListPreference) findPreference(PREF_CLOCK_DATE_DISPLAY);
         mClockDateDisplay.setOnPreferenceChangeListener(this);
@@ -193,6 +202,13 @@ public class StatusBarClockStyle extends SettingsPreferenceFragment
             int intHex = ColorPickerPreference.convertToColorInt(hex);
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.STATUSBAR_CLOCK_COLOR, intHex);
+            return true;
+        } else if (preference == mClockFontStyle) {
+            int val = Integer.parseInt((String) newValue);
+            int index = mClockFontStyle.findIndexOfValue((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUSBAR_CLOCK_FONT_STYLE, val);
+            mClockFontStyle.setSummary(mClockFontStyle.getEntries()[index]);
             return true;
         } else if (preference == mClockDateDisplay) {
             int val = Integer.parseInt((String) newValue);
