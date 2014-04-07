e96fe4634c6524cbf2b08a319ae1a5923ea08694
camcory <camcory1@gmail.com>
2014-04-07 18:31:15 -0400

    Revert "Move superuser to AndroidxSettings"
    This reverts commit 52f1887e0e841526d3c4dce33fad83bdb0c7a097.

---
+++ src/com/android/settings/androidx/superuser/MultitaskSuRequestActivity.java
@@ -0,0 +1,4 @@
+package com.android.settings.androidx.superuser;
+
+public class MultitaskSuRequestActivity extends com.koushikdutta.superuser.MultitaskSuRequestActivity {
+}
---
+++ src/com/android/settings/androidx/superuser/PackageChangeReceiver.java
@@ -0,0 +1,4 @@
+package com.android.settings.androidx.superuser;
+
+public class PackageChangeReceiver extends com.koushikdutta.superuser.PackageChangeReceiver {
+}
