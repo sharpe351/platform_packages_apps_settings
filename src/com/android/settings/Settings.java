4f3ea65786fae02b39c1502e33a9deb6210eb977
Raj Yengisetty <rajesh@cyngn.com>
2014-05-14 18:20:34 -0700

    Settings: Allow access for external apps to launch ThemeSettings Fragment directly
    Squashed commit:
    https://github.com/AOSPA/android_packages_apps_Settings/commit/db0151cf83e0f99f4b02e67b36fc7303dcc6ff7f
    
    Change-Id: I93b676d3eb570c7064eeb4217c8e420a186c8e32
    
    Conflicts:
    	AndroidManifest.xml
    	src/com/android/settings/Settings.java
    
    Conflicts:
    	AndroidManifest.xml

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1117,4 +1117,5 @@ public class Settings extends PreferenceActivity
     public static class PaymentSettingsActivity extends Settings { /* empty */ }
     public static class PrintSettingsActivity extends Settings { /* empty */ }
     public static class PrintJobSettingsActivity extends Settings { /* empty */ }
+    public static class ThemeSettingsActivity extends Settings { /* empty */ }
 }
