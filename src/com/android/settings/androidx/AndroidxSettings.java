a71059ea9eed77a7aad29464aae0ef2bcff2fec4
Dave Kessler <activethrasher00@gmail.com>
2014-11-28 16:37:21 -0500

    Customizable navbar [2/2]
    A couple of changes for androidx settings.
    
    Change-Id: I3481687dbd7bc7de903ab9266a0473b33185f5ff
    
    Conflicts:
    	res/values/vanir_strings.xml
    	res/xml/dashboard_categories.xml
    
    Conflicts:
    	res/values/custom_arrays.xml
    	res/xml/androidx_settings.xml

--- src/com/android/settings/androidx/AndroidxSettings.java
+++ src/com/android/settings/androidx/AndroidxSettings.java
@@ -1,10 +1,13 @@
 package com.android.settings.androidx;
 
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -14,12 +17,31 @@ public class AndroidxSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "AndroidxSettings";
 
+    private static final String CATEGORY_NAVBAR = "navigation_bar";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.androidx_settings);
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        try {
+            boolean hasNavBar = WindowManagerGlobal.getWindowManagerService().hasNavigationBar();
+
+            // Hide navigation bar category on devices without navigation bar
+            if (!hasNavBar) {
+                prefSet.removePreference(findPreference(CATEGORY_NAVBAR));
+            }
+        } catch (RemoteException e) {
+            Log.e(TAG, "Error getting navigation bar status");
+        }
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    } 
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         final String key = preference.getKey();
