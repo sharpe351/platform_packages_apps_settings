VanirAOSP/packages_apps_Settings

Gesture panel [1/2]
This adds a gesture panel to launch custom actions.  The panel can be
opened from the navbar or quicksettings panel.  Hardware key binding
support will be added next

Squashed commit of the following:

commit 5c3c770ce764c81d2e8b07c5c621d58d879c88ff
Author: PrimeDirective <activethrasher00@gmail.com>
Date:   Thu Aug 28 22:32:33 2014 -0400

    hide gesture menu.. it's accessible from the gesture panel now

    Change-Id: Id39b0f7bc67ebf46c6e1c7f83c746dec327afcb2

commit 7fd93ce683b136941c6a407180d905889792b4c1
Author: PrimeDirective <activethrasher00@gmail.com>
Date:   Tue Aug 26 18:29:49 2014 -0400

    QS: add Gesture panel tile

    Change-Id: I6ecb534815198204467f0bb8020640a5727b7ba5

commit b156a65dacb9579e4d588e2f870abfe3b111e9c7
Author: PrimeDirective <activethrasher00@gmail.com>
Date:   Mon Aug 25 22:26:14 2014 -0400

    Add gesture panel support

    currently only used for the navbar, will be moved to vanir actions and
    made into a QS tile and as a bindable action for hardware button devices.

    Thanks to chameleonOS for the gesture activity creater code this is based on

    Change-Id: Iacd509fa337d67b652b13ee6bc3b26ec1686c02a

Change-Id: I773190e6ea1fc415c77521502f9a0dc40f092c5f

src/com/android/settings/vanir/gesturepanel/CreateGestureActivity.java
@@ -1,0 +1,161 @@
+/*
+ * Copyright (C) 2014 VanirAOSP && the Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vanir.gesturepanel;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.MotionEvent;
+import android.gesture.GestureOverlayView;
+import android.gesture.Gesture;
+import android.gesture.GestureLibrary;
+import android.widget.TextView;
+import com.android.settings.R;
+
+public class CreateGestureActivity extends Activity {
+    private static final float LENGTH_THRESHOLD = 60.0f;
+
+    private Gesture mGesture;
+    private View mDoneButton;
+    private String mUri;
+    private String mName;
+    private boolean mIsExistingGesture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.gesture_create_gestures);
+
+        mDoneButton = findViewById(R.id.done);
+
+        final Intent intent = getIntent();
+        mUri = intent.getStringExtra("uri");
+        mName = intent.getStringExtra("name");
+
+        ((TextView) findViewById(R.id.gesture_name)).setText(mName);
+
+        GestureOverlayView overlay = (GestureOverlayView) findViewById(R.id.gestures_overlay);
+        overlay.addOnGestureListener(new GesturesProcessor());
+
+        final GestureLibrary store = GestureBuilderActivity.getStore();
+        final String gestureName = mName + '|' + mUri;
+        for (String entry : store.getGestureEntries()) {
+            if (gestureName.equals(entry)) {
+                mGesture = store.getGestures(entry).get(0);
+                mIsExistingGesture = true;
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (mGesture != null) {
+            final GestureOverlayView overlay =
+                    (GestureOverlayView) findViewById(R.id.gestures_overlay);
+            overlay.post(new Runnable() {
+                @Override
+                public void run() {
+                    overlay.setGesture(mGesture);
+                    mDoneButton.setEnabled(true);
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        
+        if (mGesture != null) {
+            outState.putParcelable("gesture", mGesture);
+        }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        
+        mGesture = savedInstanceState.getParcelable("gesture");
+        if (mGesture != null) {
+            final GestureOverlayView overlay =
+                    (GestureOverlayView) findViewById(R.id.gestures_overlay);
+            overlay.post(new Runnable() {
+                public void run() {
+                    overlay.setGesture(mGesture);
+                }
+            });
+
+            mDoneButton.setEnabled(true);
+        }
+    }
+
+    @SuppressWarnings({"UnusedDeclaration"})
+    public void addGesture(View v) {
+        if (mGesture != null) {
+            if (TextUtils.isEmpty(mName) || TextUtils.isEmpty(mUri)) {
+                return;
+            }
+
+            final GestureLibrary store = GestureBuilderActivity.getStore();
+            final String gestureName = mName + "|" +mUri;
+            if (mIsExistingGesture) {
+                store.removeEntry(gestureName);
+            }
+            store.addGesture(gestureName, mGesture);
+            store.save();
+
+            setResult(RESULT_OK);
+        } else {
+            setResult(RESULT_CANCELED);
+        }
+
+        finish();
+        
+    }
+    
+    @SuppressWarnings({"UnusedDeclaration"})
+    public void cancelGesture(View v) {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+    
+    private class GesturesProcessor implements GestureOverlayView.OnGestureListener {
+        public void onGestureStarted(GestureOverlayView overlay, MotionEvent event) {
+            mDoneButton.setEnabled(false);
+            mGesture = null;
+        }
+
+        public void onGesture(GestureOverlayView overlay, MotionEvent event) {
+        }
+
+        public void onGestureEnded(GestureOverlayView overlay, MotionEvent event) {
+            mGesture = overlay.getGesture();
+            if (mGesture.getLength() < LENGTH_THRESHOLD) {
+                overlay.clear(false);
+            }
+            mDoneButton.setEnabled(true);
+        }
+
+        public void onGestureCancelled(GestureOverlayView overlay, MotionEvent event) {
+        }
+    }
+}

src/com/android/settings/vanir/gesturepanel/GesturePanelOverlayView.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2014 VanirAOSP && the Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vanir.gesturepanel;
+
+import android.content.Context;
+import android.gesture.GestureOverlayView;
+import android.util.AttributeSet;
+
+public class GesturePanelOverlayView extends GestureOverlayView {
+    public GesturePanelOverlayView(Context context) {
+        this(context, null);
+    }
+
+    public GesturePanelOverlayView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public GesturePanelOverlayView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mClearPerformedGesture = false;
+    }
+}

sharpe351/platform_packages_apps_settings

Conflicts:
	res/values/colors.xml
	res/values/vanir_strings.xml
	res/xml/system_ui_settings.xml
	src/com/android/settings/Settings.java
	src/com/android/settings/cyanogenmod/SystemUiSettings.java
	src/com/android/settings/quicksettings/QuickSettingsUtil.java

src/com/android/settings/androidx/gesturepanel/CreateGestureActivity.java
@@ -1,0 +1,161 @@
+/*
+ * Copyright (C) 2014 VanirAOSP && the Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.androidx.gesturepanel;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.MotionEvent;
+import android.gesture.GestureOverlayView;
+import android.gesture.Gesture;
+import android.gesture.GestureLibrary;
+import android.widget.TextView;
+import com.android.settings.R;
+
+public class CreateGestureActivity extends Activity {
+    private static final float LENGTH_THRESHOLD = 60.0f;
+
+    private Gesture mGesture;
+    private View mDoneButton;
+    private String mUri;
+    private String mName;
+    private boolean mIsExistingGesture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.gesture_create_gestures);
+
+        mDoneButton = findViewById(R.id.done);
+
+        final Intent intent = getIntent();
+        mUri = intent.getStringExtra("uri");
+        mName = intent.getStringExtra("name");
+
+        ((TextView) findViewById(R.id.gesture_name)).setText(mName);
+
+        GestureOverlayView overlay = (GestureOverlayView) findViewById(R.id.gestures_overlay);
+        overlay.addOnGestureListener(new GesturesProcessor());
+
+        final GestureLibrary store = GestureBuilderActivity.getStore();
+        final String gestureName = mName + '|' + mUri;
+        for (String entry : store.getGestureEntries()) {
+            if (gestureName.equals(entry)) {
+                mGesture = store.getGestures(entry).get(0);
+                mIsExistingGesture = true;
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (mGesture != null) {
+            final GestureOverlayView overlay =
+                    (GestureOverlayView) findViewById(R.id.gestures_overlay);
+            overlay.post(new Runnable() {
+                @Override
+                public void run() {
+                    overlay.setGesture(mGesture);
+                    mDoneButton.setEnabled(true);
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        
+        if (mGesture != null) {
+            outState.putParcelable("gesture", mGesture);
+        }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        
+        mGesture = savedInstanceState.getParcelable("gesture");
+        if (mGesture != null) {
+            final GestureOverlayView overlay =
+                    (GestureOverlayView) findViewById(R.id.gestures_overlay);
+            overlay.post(new Runnable() {
+                public void run() {
+                    overlay.setGesture(mGesture);
+                }
+            });
+
+            mDoneButton.setEnabled(true);
+        }
+    }
+
+    @SuppressWarnings({"UnusedDeclaration"})
+    public void addGesture(View v) {
+        if (mGesture != null) {
+            if (TextUtils.isEmpty(mName) || TextUtils.isEmpty(mUri)) {
+                return;
+            }
+
+            final GestureLibrary store = GestureBuilderActivity.getStore();
+            final String gestureName = mName + "|" +mUri;
+            if (mIsExistingGesture) {
+                store.removeEntry(gestureName);
+            }
+            store.addGesture(gestureName, mGesture);
+            store.save();
+
+            setResult(RESULT_OK);
+        } else {
+            setResult(RESULT_CANCELED);
+        }
+
+        finish();
+        
+    }
+    
+    @SuppressWarnings({"UnusedDeclaration"})
+    public void cancelGesture(View v) {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+    
+    private class GesturesProcessor implements GestureOverlayView.OnGestureListener {
+        public void onGestureStarted(GestureOverlayView overlay, MotionEvent event) {
+            mDoneButton.setEnabled(false);
+            mGesture = null;
+        }
+
+        public void onGesture(GestureOverlayView overlay, MotionEvent event) {
+        }
+
+        public void onGestureEnded(GestureOverlayView overlay, MotionEvent event) {
+            mGesture = overlay.getGesture();
+            if (mGesture.getLength() < LENGTH_THRESHOLD) {
+                overlay.clear(false);
+            }
+            mDoneButton.setEnabled(true);
+        }
+
+        public void onGestureCancelled(GestureOverlayView overlay, MotionEvent event) {
+        }
+    }
+}

src/com/android/settings/androidx/gesturepanel/GesturePanelOverlayView.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2014 VanirAOSP && the Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.androidx.gesturepanel;
+
+import android.content.Context;
+import android.gesture.GestureOverlayView;
+import android.util.AttributeSet;
+
+public class GesturePanelOverlayView extends GestureOverlayView {
+    public GesturePanelOverlayView(Context context) {
+        this(context, null);
+    }
+
+    public GesturePanelOverlayView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public GesturePanelOverlayView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mClearPerformedGesture = false;
+    }
+}